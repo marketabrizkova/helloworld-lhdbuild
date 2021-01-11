d58a460d1790bac0f99734618247daf7acabda6b
marketabrizkova <77267725+marketabrizkova@users.noreply.github.com>
2021-01-11 18:00:52 +0100

    world

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello MLH Local Hack Day: Build!");
+            Console.WriteLine("Y´all are the best <3");
+            var name = "Markéta";
+            Console.WriteLine($"I am {name}");  
+        }
+    }
+}
